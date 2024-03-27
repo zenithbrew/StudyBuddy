2cb6bff5dd0e14c2fe7c653a3785078baaf5e321
AyushModi123 <ayushonweb785@gmail.com>
2024-03-27 22:12:00 +0530

    add user profile page

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', views.register_page, name="register"),
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<str:pk>/', views.user_profile, name="user-profile"),
     path('create-room/', views.create_room, name="create-room"),
     path('update-room/<str:pk>/', views.update_room, name='update-room'),
     path('delete-room/<str:pk>', views.delete_room, name='delete-room'),
